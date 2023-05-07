cea23ff0d65de460d3bb6aed94c473fadc14d948
Knowlton2023 <122704053+Knowlton2023@users.noreply.github.com>
2023-05-07 17:14:59 -0500

    Updates
    Adding Loops

---
+++ Week3/src/com/coderscampus/objects/Loops.java
@@ -0,0 +1,17 @@
+package com.coderscampus.objects;
+
+public class Loops {
+
+	public static void main (String[] args) {
+
+		// for Loop example code
+//		ForLoopExample forExample1 = new ForLoopExample();
+//		example1.forLoopExample();
+
+		// while loop example code
+		WhileLoopExample whileExample1 = new WhileLoopExample();
+		whileExample1.whileLoopExample1(); 
+		
+		
+	}
+}
---
+++ Week3/src/com/coderscampus/objects/WhileLoopExample.java
@@ -0,0 +1,9 @@
+package com.coderscampus.objects;
+
+public class WhileLoopExample {
+	void whileLoopExample1 () {
+		System.out.println("Inside whileLoopExample1 method");
+	}
+
+
+}
